d906281ea121529ccbdf07ccf3a0f0745e74bcf9
kirya <cloudkserg11@gmail.com>
2018-07-18 17:47:59 +0300

    https://chronobank.atlassian.net/browse/MD-483 - middleware sdk - fixed tests auth

--- config/index.js
+++ config/index.js
@@ -72,6 +72,7 @@ let config = {
         },
         laborx: {
           authProvider: 'http://localhost:3001',
+          dbAlias: 'accounts',
           profileModel: 'sdkProfile'
         }
       }
--- nodes/laborx_auth.js
+++ nodes/laborx_auth.js
@@ -8,7 +8,35 @@ const  _ = require('lodash'),
   mongoose = require('mongoose'),
   {URL} = require('url');
 
+const getAddressesFromLaborx = async (providerPath, msg) => {
+  const response = await request({
+    method: 'POST',
+    uri: new URL('signin/signature/addresses', providerPath),
+    json: true,
+    headers: {
+      'Authorization': msg.req.headers.authorization
+    }
+  });
+  if (!_.get(response, 'addresses')) 
+    throw new Error('not found addresses from auth response ' + response);
+  return response.addresses;
+};
 
+const getAddressesFromMongo = async (profileModel, token) => {
+  const profile = await profileModel.findOne({token});
+  if (profile) 
+    return profile.addresses;
+  return null;
+};
+
+const saveAddressesToMongo = async (profileModel, token, addresses) => {
+  await profileModel.findOneAndUpdate({token}, {$set: {addresses}}, {
+    upsert: true,
+    setDefaultsOnInsert: true
+  });
+};
+
+const isAuth = (msg) => { return _.get(msg.req, 'headers.authorization') !== null; };
 
 
 module.exports = function (RED) {
@@ -16,78 +44,48 @@ module.exports = function (RED) {
     RED.nodes.createNode(this, redConfig);
     let node = this;
 
-    const connection = _.get(node.context().global,`connections.${redConfig.dbAlias}`) || mongoose;
+    const ctx =  node.context().global;
+    const dbAlias = _.get(ctx.settings, 'laborx.dbAlias') || 'accounts';
+    const tableName = _.get(ctx.settings, 'laborx.profileModel') || 'ctxProfile';
+    const providerPath = redConfig.configprovider === '0' ? redConfig.providerpath : 
+      _.get(ctx.settings, 'laborx.authProvider') || 'http://localhost:3001';
+    const connection = _.get(
+      ctx,
+      `connections.primary.${dbAlias}`
+    ) || mongoose;
 
-    const getModel = function (origName) {
-      const model = connection.models[origName];
-      if (!model) 
-        throw new Error('Not find profile in mongo models');
-      return model;
-    };
-
-    const getAddressesFromLaborx = async (providerPath, msg) => {
-      const response = await request({
-        method: 'POST',
-        uri: new URL('signin/signature/addresses', providerPath),
-        json: true,
-        headers: {
-          'Authorization': msg.req.headers.authorization
-        }
-      });
-      if (!_.get(response, 'addresses')) 
-         throw new Error('not found addresses from auth response ' + response);
-      return response.addresses;
-    };
-
-    const getAddressesFromMongo = async (profileModel, token) => {
-      const profile = await profileModel.findOne({token});
-      if (profile) 
-        return profile.addresses;
-      return null;
-    };
-
-    const saveAddressesToMongo = async (profileModel, token, addresses) => {
-      await profileModel.findOneAndUpdate({token}, {$set: {addresses}}, {
-        upsert: true,
-        setDefaultsOnInsert: true
-      });
-    };
-
-    const isAuth = (msg) => _.get(msg, 'req.headers.authorization');
 
     this.on('input', async function (msg) {
+      let models = (connection).modelNames();
+      let origName = _.find(models, m => m.toLowerCase() === tableName.toLowerCase());
+      if (!origName) 
+        return node.error('not found profileModel in connections', 'not right profileModel');
 
-      const ctx =  node.context().global;
-      const profileModel = getModel( _.get(ctx.settings, 'laborx.profileModel'));
-
-      const providerPath = redConfig.configprovider === '0' ? redConfig.providerpath : 
-        _.get(ctx.settings, 'laborx.authProvider') || 'http://localhost:3001';
+      const profileModel = connection.models[origName];
       if (!isAuth(msg)) {
         msg.statusCode = '400';
-        return this.error('Not set authorization headers', msg);
+        return node.error('Not set authorization headers', msg);
       }
 
       const authorization = _.get(msg, 'req.headers.authorization');
       const params = authorization.split(' ');
       if (params[0] === 'Bearer') {
-        msg.addresses =await getAddressesFromMongo(profileModel, params[1]);
-        if (msg.addresses) {
-          node.send(msg);
-          return;
-        }
+        msg.addresses = await getAddressesFromMongo(profileModel, params[1]);
+        if (msg.addresses) 
+          return node.send(msg);
       }
 
       try {
         let addresses = await getAddressesFromLaborx(providerPath, msg);
-        
         msg.addresses = addresses;
         if (params[0] === 'Bearer') 
           await saveAddressesToMongo(profileModel, params[1], addresses);
-        node.send(msg);
       } catch (err) {
         msg.statusCode = '401';
-        this.error(err, msg);
+        return node.error(err, msg);
       }
+
+      node.send(msg);
     });
   }
 
