90684036d84378d95f18e2d277c839a6889ace59
kirya <cloudkserg11@gmail.com>
2018-08-14 11:57:27 +0300

    https://chronobank.atlassian.net/browse/MD-580 - fixed tests

--- tests/processes/laborxProxy.js
+++ tests/processes/laborxProxy.js
@@ -14,7 +14,7 @@ app.post('/signin/signature/addresses', function(req, res) {
   
   const params = req.headers.authorization.split(' ');
   console.log('token = ' + params[0] + ' value=' + params[1] + '?=' + config.dev.signature) ;
-  if (params[0] === 'Bearer' && params[1] === config.dev.signature) {
+  if (params[0] === 'Bearer' && params[1] == config.dev.signature) {
     res.status(200).send(JSON.stringify({
         'addresses': {
           'ethereum-public-key': config.dev['ethereum-public-key'],
